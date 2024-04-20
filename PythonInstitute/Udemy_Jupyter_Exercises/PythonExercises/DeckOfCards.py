1b0b4b413745bbe23e24b2b94f00fb35bcd92e13
Tony-Acosta <Antonio.Acosta@ibm.com>
2024-04-19 18:03:48 -0600

    Functional shuffle method on Deck class

---
+++ PythonInstitute/Udemy_Jupyter_Exercises/PythonExercises/DeckOfCards.py
@@ -0,0 +1,52 @@
+import random
+
+
+class Card:
+    def __init__(self, suit, value):
+        self.suit = suit
+        self.value = value
+
+    def present(self):
+        return self.value + ' of ' + self.suit
+
+
+class Deck:
+
+    def __init__(self):
+        suits = ['hearts', 'diamonds', 'clubs', 'spades']
+        values = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
+        self.cards = [Card(suit, value) for suit in suits for value in values]
+
+    def shuffle(self):
+        for i in range(len(self.cards)):
+            random_index = random.randint(0, len(self.cards)-1)
+            temp_var = self.cards[random_index]
+            self.cards[random_index] = self.cards[i]
+            self.cards[i] = temp_var
+
+    def deal(self):
+        if len(self.cards) != 0:
+            deal = self.cards[-1]
+            del self.cards[-1]
+            return deal
+        else:
+            return None
+
+    def count_remaining(self):
+        return len(self.cards)
+
+    def get_remaining(self):
+        return [i.present() for i in self.cards]
+
+
+deck_one = Deck()
+
+# showing the just created cards in Deck:
+for i in deck_one.get_remaining():
+    print(i)
+
+deck_one.shuffle()
+
+print('\nShowing the shuffled cards in Deck:')
+for i in deck_one.get_remaining():
+    print(i)
