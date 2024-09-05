a8ee8e0ba43c40d9236d994fc1cc4332e821db87
ANTONIO-ACOSTA <Antonio.Acosta@ibm.com>
2024-09-04 20:58:04 -0600

    unpacking arguments for the executor function

--- tests/TestExecutor.py
+++ tests/TestExecutor.py
@@ -4,6 +4,6 @@ from datetime import datetime
 def execute_function(test_input, function):
     for i in test_input:
         start_time = datetime.now()
-        print(function(i))
+        print(function(*i))
         print('Elapsed time:', datetime.now() - start_time)
         print()
