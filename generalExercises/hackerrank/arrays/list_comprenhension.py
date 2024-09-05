edd6addafdeff80c1a40c94c69620617a44e82ee
ANTONIO-ACOSTA <Antonio.Acosta@ibm.com>
2024-09-04 20:59:53 -0600

    delete unesessary order functions

--- generalExercises/hackerrank/arrays/list_comprenhension.py
+++ generalExercises/hackerrank/arrays/list_comprenhension.py
@@ -3,15 +3,6 @@ from tests import TestExecutor
 
 def coordinates(x, y, z, n):
 
-    def sort_first(my_list):
-        return my_list[0]
-
-    def sort_second(my_list):
-        return my_list[1]
-
-    def sort_third(my_list):
-        return my_list[2]
-
     valid_coordinates = list(filter(lambda x: sum(x) != n,
                                     [[i, j, k] for k in range(0, z + 1) for j in range(0, y + 1) for i in range(0, x + 1)]))
 
