361e4a6dc35a4c67699c80e2530cde9593da79f7
ANTONIO-ACOSTA <Antonio.Acosta@ibm.com>
2024-09-10 17:04:55 -0600

    Include heapq inbuilt function usage

--- dataStructures/NonLinear/trees/heaps/Heap.py
+++ dataStructures/NonLinear/trees/heaps/Heap.py
@@ -7,10 +7,11 @@ class Heap:
 
     def insert_in_heap(self, element):
         if self._current_size == self._max_size:
-            print('No space in heap!')
+            print('Heap is full!')
             return
         self._current_size += 1
         heap_index = self._current_size
+
         while heap_index > 1 and element > self._data[heap_index//2]:
             self._data[heap_index] = self._data[heap_index//2]
             heap_index //= 2
@@ -20,6 +21,7 @@ class Heap:
         if self._current_size == 0:
             print('Heap is empty!')
             return
+
         element = self._data[1]
         self._data[1] = self._data[self._current_size]
         self._data[self._current_size] = -1
@@ -27,12 +29,12 @@ class Heap:
         i = 1
         j = i * 2
         while j < self._current_size:
-            if self._data[j] < self._data[j + 1]:
+            if self._data[j + 1] > self._data[j]:
                 j += 1
-            if self._data[i] < self._data[j]:
-                temp = self._data[i]
-                self._data[i] = self._data[j]
-                self._data[j] = temp
+            if self._data[j] > self._data[i]:
+                temp = self._data[j]
+                self._data[j] = self._data[i]
+                self._data[i] = temp
                 i = j
                 j = i * 2
             else:
@@ -55,23 +57,51 @@ class Heap:
         return len(self._data) == 0
 
 
-h = Heap()
-h.insert_in_heap(25)
-h.insert_in_heap(14)
-h.insert_in_heap(2)
-h.insert_in_heap(20)
-h.insert_in_heap(10)
-h.insert_in_heap(40)
-print(h.get_data())
-print(h.delete_max())
-print(h.get_data())
-print(h.delete_max())
-print(h.get_data())
-print(h.delete_max())
-print(h.get_data())
-print(h.delete_max())
-print(h.get_data())
-print(h.delete_max())
-print(h.get_data())
-print(h.delete_max())
-print(h.get_data())
+if __name__ == '__main__':
+    h = Heap()
+    h.insert_in_heap(25)
+    h.insert_in_heap(14)
+    h.insert_in_heap(2)
+    h.insert_in_heap(20)
+    h.insert_in_heap(10)
+    h.insert_in_heap(40)
+    print(h.get_data())
+    print(h.delete_max())
+    print(h.get_data())
+    print(h.delete_max())
+    print(h.get_data())
+    print(h.delete_max())
+    print(h.get_data())
+    print(h.delete_max())
+    print(h.get_data())
+    print(h.delete_max())
+    print(h.get_data())
+    print(h.delete_max())
+    print(h.get_data())
+
+    import heapq as heap
+
+    print('\nHeap by using heapq inbuilt python module:')
+    L1 = []
+    L2 = [20, 4, 2, 90, 45, 4, 14]
+    heap.heappush(L1, 25)
+    heap.heappush(L1, 14)
+    heap.heappush(L1, 2)
+    heap.heappush(L1, 20)
+    heap.heappush(L1, 10)
+    heap.heappush(L1, 40)
+    print(L1)
+    e = heap.heappop(L1)
+    print(e)
+    print(L1)
+    e = heap.heappushpop(L1, 1)
+    print(e)
+    print(L1)
+    e = heap.heapreplace(L1, 1)
+    print(e)
+    print(L1)
+
+    print('\nConverting a list into a heap:')
+    print(L2)
+    heap.heapify(L2)
+    print(L2)
