35624feee85e047828c176dcd37477b6d0762dee
ANTONIO-ACOSTA <Antonio.Acosta@ibm.com>
2024-09-27 10:32:13 -0600

    Change failed tc alert to Test Executor

--- tests/TestExecutor.py
+++ tests/TestExecutor.py
@@ -15,7 +15,7 @@ def execute_function(test_inputs, expected_values, function):
             failed_tc_list.append(i+1)
 
     if len(failed_tc_list) > 0:
-        print('\n---> These are the failing TCs:')
+        print('\n---> TEST SUITE FAILED\n---> Failing TCs:')
         for i in failed_tc_list:
             print(i)
 
