9c2aa22c9cc9fb9c3f80e8dd09ec18adb9da1553
ANTONIO-ACOSTA <Antonio.Acosta@ibm.com>
2024-09-27 10:20:28 -0600

    Change failed tc traceability to Test Executor

--- tests/TestExecutor.py
+++ tests/TestExecutor.py
@@ -1,9 +1,21 @@
 from datetime import datetime
 
 
-def execute_function(test_input, function):
-    for i in test_input:
+def execute_function(test_inputs, expected_values, function):
+
+    failed_tc_list = []
+    for i in range(len(test_inputs)):
         start_time = datetime.now()
-        print('Function return:', function(*i))
+        print('Function return:', function(*test_inputs[i]))
         print('Elapsed time:', datetime.now() - start_time)
-        print()
+        if function(*test_inputs[i]) == expected_values[i]:
+            print('Test:', i+1, 'PASSED')
+        else:
+            print('Test:', i+1, 'FAILED')
+            failed_tc_list.append(i+1)
+
+    if len(failed_tc_list) > 0:
+        print('\n---> These are the failing TCs:')
+        for i in failed_tc_list:
+            print(i)
+
