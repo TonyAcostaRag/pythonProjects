e716b25791b2628690434ac30e6de393762b37db
ANTONIO-ACOSTA <Antonio.Acosta@ibm.com>
2024-09-27 13:48:17 -0600

    Traceability between expected values and actual results

--- tests/TestExecutor.py
+++ tests/TestExecutor.py
@@ -3,19 +3,19 @@ from datetime import datetime
 
 def execute_function(test_inputs, expected_values, function):
 
-    failed_tc_list = []
+    failed_tc_dict = {}
     for i in range(len(test_inputs)):
         start_time = datetime.now()
         print('Function return:', function(*test_inputs[i]))
         print('Elapsed time:', datetime.now() - start_time)
-        if function(*test_inputs[i]) == expected_values[i]:
+        actual_result = function(*test_inputs[i])
+        if actual_result == expected_values[i]:
             print('Test:', i+1, 'PASSED\n')
         else:
             print('Test:', i+1, 'FAILED\n')
-            failed_tc_list.append(i+1)
+            failed_tc_dict[(i+1)] = actual_result
 
-    if len(failed_tc_list) > 0:
+    if len(failed_tc_dict) > 0:
         print('\n---> TEST SUITE FAILED\n---> Failing TCs:')
-        for i in failed_tc_list:
-            print(i)
-
+        for key, value in failed_tc_dict.items():
+            print('Test case:', key, 'Result:', value)
