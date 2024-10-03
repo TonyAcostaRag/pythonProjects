c9ba0cd90cc578300bab39856c2b41662a4cc9ea
ANTONIO-ACOSTA <Antonio.Acosta@ibm.com>
2024-10-03 00:14:52 -0600

    Include printing test input to TestExecutor.py

--- tests/TestExecutor.py
+++ tests/TestExecutor.py
@@ -6,9 +6,10 @@ def execute_function(test_inputs, expected_values, function):
     failed_tc_dict = {}
     for i in range(len(test_inputs)):
         start_time = datetime.now()
-        print('Actual result:', function(*test_inputs[i]))
+        print('  Actual result:', function(*test_inputs[i]))
         print('Expected result:', expected_values[i])
         print('Elapsed time:', datetime.now() - start_time)
+        print('Test input:', *test_inputs[i])
         actual_result = function(*test_inputs[i])
         if actual_result == expected_values[i]:
             print('Test:', i+1, 'PASSED\n')
@@ -20,3 +21,5 @@ def execute_function(test_inputs, expected_values, function):
         print('\n---> TEST SUITE FAILED\n---> Failing TCs:')
         for key, value in failed_tc_dict.items():
             print('Test case:', key, 'Result:', value)
+    else:
+        print('\n---> ALL TEST SUITE PASSED')
