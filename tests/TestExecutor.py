1ed8add7c426584380a6d4c402603661b19f7a1c
ANTONIO-ACOSTA <Antonio.Acosta@ibm.com>
2024-10-10 18:53:25 -0600

    Add test results into one simgle print statement

--- tests/TestExecutor.py
+++ tests/TestExecutor.py
@@ -6,10 +6,12 @@ def execute_function(test_inputs, expected_values, function):
     failed_tc_dict = {}
     for i in range(len(test_inputs)):
         start_time = datetime.now()
-        print('\n  Actual result:', function(*test_inputs[i]))
-        print('Expected result:', expected_values[i])
-        print('Elapsed time:', datetime.now() - start_time)
-        print('Test input:', *test_inputs[i])
+        print(
+            '\n  Actual result:', function(*test_inputs[i]),
+            'Expected result:', expected_values[i],
+            'Elapsed time:', datetime.now() - start_time,
+            'Test input:', *test_inputs[i]
+        )
         actual_result = function(*test_inputs[i])
         if actual_result == expected_values[i]:
             print('Test:', i+1, 'PASSED\n')
