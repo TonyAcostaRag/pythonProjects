083f8e7e23f85596075f39ae22186e9e2e00feae
ANTONIO-ACOSTA <Antonio.Acosta@ibm.com>
2024-10-11 19:59:51 -0600

    Functional two_sum_II.py

--- generalExercises/leetcode/arrays_and_strings/two_sum_II.py
+++ generalExercises/leetcode/arrays_and_strings/two_sum_II.py
@@ -19,9 +19,9 @@ def two_sum(numbers: list[int], target: int) -> list[int]:
 
 if __name__ == '__main__':
     TestExecutor.execute_function([
-        [[2,7,11,15], 9],
-        [[2,3,4], 6],
-        [[-1,0], -1],
+        [[2, 7, 11, 15], 9],
+        [[2, 3, 4], 6],
+        [[-1, 0], -1],
         [[2, 3, 5], 6]
     ],
     [
