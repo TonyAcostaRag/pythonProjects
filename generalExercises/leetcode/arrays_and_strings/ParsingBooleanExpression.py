0918f3a3227e106eceba5929db79882f166080aa
ANTONIO-ACOSTA <Antonio.Acosta@ibm.com>
2024-10-24 19:12:56 -0600

    Functional ParsingBooleanExpression.py

--- generalExercises/leetcode/arrays_and_strings/ParsingBooleanExpression.py
+++ generalExercises/leetcode/arrays_and_strings/ParsingBooleanExpression.py
@@ -1,102 +1,89 @@
 from tests import TestExecutor
 
 
-def parse_bool_expr(expression: str) -> bool:
+def parse_bool_expr(expression: str):
 
-    def inner_expression_evaluator(expr: str):
-        print('\nInput expr:', expr)
-        boolean_operands = []
-        boolean_operator = ''
-        inner_evaluation = ''
-        index_inner_operator = 0
-        for i in range(len(expr)):
-            if expr[i] == ')':
+    def evaluate_single_expression(single_expression):
+        if len(single_expression) == 1:
+            return True if single_expression == 't' else False
+        operator = single_expression[0]
+        operand = ''
+        for i in range(2, len(single_expression) -1):
+            operand += single_expression[i]
+        if operator == '&':
+            return False if 'f' in operand else True
+        elif operator == '|':
+            return True if 't' in single_expression else False
+        elif operator == '!':
+            return True if operand == 'f' else False
+
+    def is_single_operation(expression):
+        for i in range(1, len(expression)):
+            if expression[i] == '&' or expression[i] == '!' or expression[i] == '|':
+                return False
+        return True
+
+    def get_inner_operators(expression):
+
+        for i in range(2, len(expression) - 1):
+            if expression[i] in ['&', '|', '!']:
                 break
-            if expr[i] == '&' or expr[i] == '|' and boolean_operator != '':
-                boolean_operands.clear()
-                boolean_operator = ''
-            if expr[i] == 'f' or expr[i] == 't':
-                boolean_operands.append(expr[i])
-                if boolean_operator == '':
-                    boolean_operator = expr[i - 2: i - 1]
-                    index_inner_operator = i - 2
+        else:
+            return None, expression
 
-        if boolean_operator == '&':
-            if 'f' in boolean_operands:
-                inner_evaluation = 'f'
-            else:
-                inner_evaluation = 't'
+        expressions_list = []
+        single_exp = ''
+        parenthesis_count = 0
+        for i in range(2, len(expression) - 1):
+            if (expression[i] == ','
+                    and (expression[i - 1] == ')'
+                         or expression[i - 1] == 'f'
+                         or expression[i - 1] == 't')):
+                continue
+            single_exp += expression[i]
+            if expression[i] == '(':
+                parenthesis_count += 1
+            elif expression[i] == ')':
+                parenthesis_count -= 1
+            elif parenthesis_count == 0 and (expression[i] == 't' or expression[i] == 'f'):
+                expressions_list.append(single_exp)
+                single_exp = ''
+            if parenthesis_count == 0 and len(single_exp) > 2 and '(' in single_exp:
+                expressions_list.append(single_exp)
+                single_exp = ''
 
-        elif boolean_operator == '!':
-            if boolean_operands[0] == 'f':
-                inner_evaluation = 't'
-            else:
-                inner_evaluation = 'f'
+        return expression[:2], expressions_list
 
-        elif boolean_operator == '|':
-            if 't' in boolean_operands:
-                inner_evaluation = 't'
-            else:
-                inner_evaluation = 'f'
+    outer_operator, inner_operators = get_inner_operators(expression)
 
-        print(
-            '\nboolean_operands:', boolean_operands,
-            '\nboolean_operator:', boolean_operator,
-            '\nindex_inner_operator:', index_inner_operator,
-            '\nFirst part:', expr[:index_inner_operator],
-            '\nSecond part:', inner_evaluation,
-            '\nThird part:', expr.replace(',', '')[index_inner_operator + 3 + len(boolean_operands):],
-            '\nWhole string:', expr[:index_inner_operator] + inner_evaluation + expr.replace(',', '')[index_inner_operator + 3 + len(boolean_operands):]
-        )
+    if is_single_operation(expression):
+        return evaluate_single_expression(inner_operators)
 
-        expr = expr[:index_inner_operator] + inner_evaluation + expr.replace(',', '')[index_inner_operator + 3 + len(boolean_operands):]
-        return [
-            True if index_inner_operator == 0 else False,
-            expr
-        ]
+    evaluated_operations = ''
+    for inner_op in inner_operators:
+        if is_single_operation(inner_op):
+            evaluated_operations += 't' if evaluate_single_expression(inner_op) else 'f'
+        else:
+            evaluated_operations += 't' if parse_bool_expr(inner_op) else 'f'
 
-    expression = inner_expression_evaluator(expression)
-    if expression[0]:
-        return True if expression[1] == 't' else False
-
-    return parse_bool_expr(expression[1])
+    return parse_bool_expr(outer_operator + evaluated_operations + ')')
 
 
 if __name__ == '__main__':
     TestExecutor.execute_function([
-        #["&(|(f))"],
-        #["|(f,f,f,t)"],
-        #["!(&(f,t))"],
-        #["|(&(t,f,t),t)"],
-        #["|(f,&(t,t))"],
-        [
-            "!("
-                "&("
-                    "!(t),"
-                    "!("
-                        "!(&(f))"
-                    "),"
-                    "&("
-                        "&("
-                            "!(&(f)),"
-                            "&(t),"
-                            "|(f,f,t)"
-                        "),"
-                        "&(t),"
-                        "&(t,t,f)"
-                    ")"
-                ")"
-            ")"
-        ]
-        # Approach: Tomar todas las funciones dentro de una funcion iterando el string, y cada una de las inner
-        # funcions procesarlas recursivamente con el approach divide and conquer.
+        ["&(|(f))"],
+        ["|(f,f,f,t)"],
+        ["!(&(f,t))"],
+        ["|(&(t,f,t),t)"],
+        ["|(f,&(t,t))"],
+        ["!(&(!(t),!(!(&(f))),&(&(!(&(f)),&(t),|(f,f,t)),&(t),&(t,t,f))))"]
     ],
     [
-        #False,
-        #True,
-        #True,
-        #True,
-        #True,
+        False,
+        True,
+        True,
+        True,
+        True,
         True
     ],
         parse_bool_expr
