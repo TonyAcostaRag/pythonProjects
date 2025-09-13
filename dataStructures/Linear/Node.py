4b65db0b671b3cd40b855b9efa06db167a2ea6c4
ANTONIO-ACOSTA <Antonio.Acosta@ibm.com>
2025-09-13 15:41:41 -0600

    Add double linked list and tree dual nodes

--- dataStructures/Linear/Node.py
+++ dataStructures/Linear/Node.py
@@ -1,22 +1,40 @@
-class _Node:
-    __slots__ = '_value', '_next'
+class Node:
+    __slots__ = 'value', 'next'
 
     def __init__(self, value):
-        self._value = value
-        self._next = None
+        self.value = value
+        self.next = None
+
+
+class DualNode:
+    __slots__ = 'value', 'next', 'prev'
+
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
+
+
+class TreeNode:
+    __slots__ = 'value', 'left', 'right'
+
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
 
 
 if __name__ == '__main__':
 
-    node_one = _Node(1)
+    node_one = Node(1)
 
-    node_two = _Node(2)
-    node_one._next = node_two
+    node_two = Node(2)
+    node_one.next = node_two
 
-    node_three = _Node(3)
-    node_two._next = node_three
+    node_three = Node(3)
+    node_two.next = node_three
 
-    print(node_one._value)
-    print(node_one._next._value)
-    print(node_one._next._next._value)
-    print(node_one._next._next._next)
+    print(node_one.value)
+    print(node_one.next.value)
+    print(node_one.next.next.value)
+    print(node_one.next.next.next)
