17bd50e48d34535765953e613689103fe698e5e5
ANTONIO-ACOSTA <Antonio.Acosta@ibm.com>
2025-09-22 17:32:06 -0600

    add functional BST methods

--- dataStructures/NonLinear/tree/BinarySearchTree.py
+++ dataStructures/NonLinear/tree/BinarySearchTree.py
@@ -67,9 +67,141 @@ class BinarySearchTree:
         else:
             return True
 
+    def min_value(self, node=None):
+        # The recursive methods uses more memory than iterative methods.
+
+        if node is None:
+            node = self.root
+
+        if node.left is None:
+            return node.value
+        else:
+            return self.min_value(node.left)
+
+    def delete_node(self, value, node):
+        
+        if node is None:
+            return None
+        
+        if value == self.root.value:
+            self.root = None
+
+        if value < node.value:
+            node.left = self.delete_node(value, node.left)
+        elif value > node.value:
+            node.right = self.delete_node(value, node.right)
+        else:
+            if node.left is None and node.right is None:
+                return None
+            elif node.left is None:
+                node = node.right
+            elif node.right is None:
+                node = node.left
+            else:
+                sub_tree_min_val = self.min_value(node.right)
+                node.value = sub_tree_min_val
+                node.right = self.delete_node(node.right, sub_tree_min_val)
+        return node
+        
+    def sorted_list_to_bst(self, nums):
+        self.root = self.__sorted_list_to_bst(nums, 0, len(nums) - 1)
+
+    def __sorted_list_to_bst(self, nums, left, right):
+        
+        if left > right:
+            return None
+        
+        if (left + right) % 2 == 0:
+            middle_ele = int((left + right) / 2)
+        else:
+            middle_ele = ((left + right) // 2) + 1
+
+        node = TreeNode(nums[middle_ele])
+        
+        node.left  = self.__sorted_list_to_bst(nums, left, middle_ele - 1)
+        node.right = self.__sorted_list_to_bst(nums, middle_ele + 1, right)
+        
+        return node
+    
+    def invert(self):
+        self.root = self.__invert(self.root)
+
+    def __invert(self, node):
+        if node is None:
+            return None
+        
+        temp = node.left
+        node.left = node.right
+        node.right = temp
+
+        node.left = self.__invert(node.left)
+        node.right = self.__invert(node.right)
+
+        return node
+    
+    def tree_traversal_BFS(self):
+        queue = []
+        result = []
+        queue.append(self.root)
+        while queue:
+            processing_node = queue.pop(0)
+            if processing_node:
+                queue.append(processing_node.left)
+                queue.append(processing_node.right)
+                result.append(processing_node.value)
+
+        return result
+
+    def tree_traversal_DFS(self):
+        stack = []
+        result = []
+        stack.append(self.root)
+        while stack:
+            processing_node = stack.pop()
+            if processing_node:
+                stack.append(processing_node.left)
+                stack.append(processing_node.right)
+                result.append(processing_node.value)
+
+        return result
+    
+    def tree_traversal_DFS_PreOrder_call_stack(self):
+        result = []
+        def traversal(node):
+            if node:
+                result.append(node.value)
+                traversal(node.left)
+                traversal(node.right)
+            
+        traversal(self.root)
+        return result
+
+    def tree_traversal_DFS_PostOrder_call_stack(self):
+        result = []
+        def traversal(node):
+            if node:
+                traversal(node.left)
+                traversal(node.right)
+                result.append(node.value)
+        
+        traversal(self.root)
+        return result
+
+    def tree_traversal_DFS_InOrder_call_stack(self):
+        result = []
+        def traversal(node):
+            if node:
+                traversal(node.left)
+                result.append(node.value)
+                traversal(node.right)
+        
+        traversal(self.root)
+        return result
+
 
 if __name__ == '__main__':
 
+    '''
     my_binary_search_tree = BinarySearchTree()
     # Inserting nodes to my BST:
     print('Printing root node:', my_binary_search_tree.root)
@@ -82,10 +214,90 @@ if __name__ == '__main__':
     print('Inserting a node:', my_binary_search_tree.insert_node(76))
     print('Printing root\'s right node:', my_binary_search_tree.root.right.value)
 
+    print('Inserting a node:', my_binary_search_tree.insert_node(18))
+    print('Inserting a node:', my_binary_search_tree.insert_node(60))
+
     print('Trying inserting a duplicated node:', my_binary_search_tree.insert_node(76))
 
     # Checking whether a value exist within the BST:
     print('Does the tree contain the 47?:', my_binary_search_tree.contains_node(47))
     print('Does the tree contain the 21?:', my_binary_search_tree.contains_node(21))
+    print('Does the tree contain the 21?:', my_binary_search_tree.contains_node(18))
     print('Does the tree contain the 76?:', my_binary_search_tree.contains_node(76))
-    print('Does the tree contain the 50?:', my_binary_search_tree.contains_node(50))
+    print('Does the tree contain the 50?:', my_binary_search_tree.contains_node(60))
+    print('Does the tree contain the 21?:', my_binary_search_tree.contains_node(77))
+    print('Does the tree contain the 21?:', my_binary_search_tree.contains_node(70))
+    print('Does the tree contain the 21?:', my_binary_search_tree.contains_node(78))
+
+    # Cheking the BST minimum value:
+    print('BST minimum value from root:', my_binary_search_tree.min_value())
+    print('BST minimum value from specified node:', my_binary_search_tree.min_value(my_binary_search_tree.root.right))
+
+    # Deleting nodes:
+    my_binary_search_tree.delete_node(18, my_binary_search_tree.root)
+    print('Values after deleting 18:', my_binary_search_tree.root.left.left)
+
+    print('BST minimum value from root:', my_binary_search_tree.min_value())
+    '''
+
+    '''
+    # Convert a sorted list into a BST
+    my_empty_binary_search_tree = BinarySearchTree()
+    #my_empty_binary_search_tree.sorted_list_to_bst([])
+    #my_empty_binary_search_tree.sorted_list_to_bst([10])
+    my_empty_binary_search_tree.sorted_list_to_bst([1, 2, 3, 4, 5])
+    print('Root:', my_empty_binary_search_tree.root.value)
+    print('Root\'s left node:', my_empty_binary_search_tree.root.left.value)
+    print('Root\'s left node:', my_empty_binary_search_tree.root.left.left.value)
+    print('Root\'s left node:', my_empty_binary_search_tree.root.left.left.left)
+    print('Root\'s left node:', my_empty_binary_search_tree.root.left.left.right)
+    print('Root\'s left node:', my_empty_binary_search_tree.root.left.right)
+    print('Root\'s right node:', my_empty_binary_search_tree.root.right.value)
+    print('Root\'s right node:', my_empty_binary_search_tree.root.right.left.value)
+    print('Root\'s right node:', my_empty_binary_search_tree.root.right.left.left)
+    print('Root\'s right node:', my_empty_binary_search_tree.root.right.left.right)
+    print('Root\'s right node:', my_empty_binary_search_tree.root.right.right)
+    '''
+
+    '''
+    # Invert a BST
+    my_inverted_BST = BinarySearchTree()
+    my_inverted_BST.insert_node(47)
+    my_inverted_BST.insert_node(21)
+    my_inverted_BST.insert_node(76)
+    my_inverted_BST.insert_node(18)
+    my_inverted_BST.insert_node(27)
+    my_inverted_BST.insert_node(52)
+    my_inverted_BST.insert_node(82)
+
+    my_inverted_BST.invert()
+    print(my_inverted_BST.root.left.left.value)
+    print(my_inverted_BST.root.right.right.value)
+    '''
+
+    
+    # BFS Traversal
+    my_BFS_traversal_tree = BinarySearchTree()
+    my_BFS_traversal_tree.insert_node(47)
+    my_BFS_traversal_tree.insert_node(21)
+    my_BFS_traversal_tree.insert_node(76)
+    my_BFS_traversal_tree.insert_node(18)
+    my_BFS_traversal_tree.insert_node(27)
+    my_BFS_traversal_tree.insert_node(52)
+    my_BFS_traversal_tree.insert_node(82)
+    print('\nBFS tree traversal:\n', my_BFS_traversal_tree.tree_traversal_BFS())
+
+    # DFS Post Order Traversal
+    my_DFS_traversal_tree = BinarySearchTree()
+    my_DFS_traversal_tree.insert_node(47)
+    my_DFS_traversal_tree.insert_node(21)
+    my_DFS_traversal_tree.insert_node(76)
+    my_DFS_traversal_tree.insert_node(18)
+    my_DFS_traversal_tree.insert_node(27)
+    my_DFS_traversal_tree.insert_node(52)
+    my_DFS_traversal_tree.insert_node(82)
+    print('\nDFS tree traversal:\n', my_DFS_traversal_tree.tree_traversal_DFS())
+    print('\nDFS Pre Order tree traversal (call stack):\n', my_DFS_traversal_tree.tree_traversal_DFS_PreOrder_call_stack())
+    print('\nDFS Post Order tree traversal (call stack):\n', my_DFS_traversal_tree.tree_traversal_DFS_PostOrder_call_stack())
+    print('\nDFS In Order tree traversal (call stack):\n', my_DFS_traversal_tree.tree_traversal_DFS_InOrder_call_stack())
+    
